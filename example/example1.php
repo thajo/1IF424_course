a71381450c2fdd3805ad7b585664c6403463b022
John Häggerud <thajo@lnu.se>
2013-03-25 17:14:30 +0100

    F01
    Uppdaterat föreläsningarna ytterligare

---
+++ example/example1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+    <meta charset=utf-8>
+    <title>Ett html-dokument i en php-fil</title>
+  </head>
+  <body>
+  <?php
+	// Vårt första php-skript
+	echo "<div>Hallå världen!</div>";
+	echo "<p>" .(9 / 3) ."</p>";	
+   ?>
+
+   echo "<p>" .(9 / 3) ."</p>";	
+
+	
+</body>	
+</html>
+
+
