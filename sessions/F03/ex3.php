153cd514e4b9912386278e09211ae64c1fcf8fc8
John H <thajo@lnu.se>
2013-04-09 06:31:48 +0200

    Exempel uppdatering
    Uppdaterat exempel

--- sessions/F03/ex3.php
+++ sessions/F03/ex3.php
@@ -2,43 +2,69 @@
 	
 <html>
 <head>
-	<title>Exempel 1</title>
+	<title>Exempel 3</title>
 	<meta charset="utf-8" />
 </head>
 <body>
 
-<form method="post">
-	Skriv meddelande: <br /><textarea rows="4" cols="50" name="message"></textarea><br />
-	Skriv hur många: <br /><input type="text" name="how_many" /><br />
-	<input type="submit" name="submit" value="Skicka" />
-</form>
-
 <?php
 	
+	// Hjälp med brevskrivning angående tenta
 	
-
-	if( isset($_POST["submit"]) ) {
-
-		
-		$mess = htmlspecialchars($_POST["message"]);
-		$nr = $_POST["how_many"];
-		
-		checkIfEmpty($mess);
-		
-		echo $mess;
-		
-		
-		
-		if($nr != "" && is_numeric($nr)) {
-			echo "beräkingen gav: " .$nr + 10;
-		}
-		else {
-			echo "Det du skrev var ingen siffra";
-		}
-		
-		
-		
-	}
+	// Vad händer om vi ändrar betygsystem
+	
+	
+/*	$mail = "Hej John!";
+	$mail .= "Din poäng på tentan var 15.";
+	$mail .= "Det ger betyget 3.";
+	$mail .= "Hej då!";
+	echo $mail ."<br /><hr />";
+	
+	$mail = "Hej Greta!";
+	$mail .= "Din poäng på tentan var 35.";
+	$mail .= "Det ger betyget 4.";
+	$mail .= "Hej då!";
+	echo $mail ."<br /><hr />";
+	
+	$mail = "Hej Lisa!";
+	$mail .= "Din poäng på tentan var 45.";
+	$mail .= "Det ger betyget 5.";
+	$mail .= "Hej då!";
+	echo $mail ."<br /><hr />";*/
+	
+	
+	writeMail("John", 15);
+	writeMail("Greta", 35);
+	writeMail("Lisa", 45);	
+	
+	
+	
 ?>
 </body>
 </html>
+
+<?php
+function writeMail($name, $point) {
+	$mail = "Hej $name!";
+	$mail .= "Din poäng på tentan var $points.";
+	$grade = getGrade($point);
+	
+	$mail .= "Det ger betyget: $grade.";
+	$mail .= "Hej då!";
+	echo $mail ."<br /><hr />";
+}
+
+function getGrade($p) {
+	if($p >= 40) {
+		return "MVG";
+	}
+	elseif($p >= 30) {
+		return "VG";
+	}
+	elseif($p >= 15) {
+		return "G";
+	}
+	else {
+		return "U";
+	}
+}
